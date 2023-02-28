540286397238df23ee415ea6f7fa9e1e96c6c10c
ABDUL HANAN <a.hanan.official@gmail.com>
2023-02-28 22:13:15 +0500

    update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAVK93SEliDxdwBuDtVHmm_xeBTy3RuvRM",
-  authDomain: "clone-fc276.firebaseapp.com",
-  projectId: "clone-fc276",
-  storageBucket: "clone-fc276.appspot.com",
-  messagingSenderId: "471739835743",
-  appId: "1:471739835743:web:05fbfa5c9c5b90e00fc2ea",
-  measurementId: "G-F9EXY81RQC",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
